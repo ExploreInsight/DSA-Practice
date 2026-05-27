e5715da220b153aca3225a5c6434adde4f1b3d3e
ExploreInsight <chiragchandel1016@gmail.com>
2026-05-27 09:21:44 +0530

    Fix alternating merge logic in for-loop

--- merge-strings-alternately.js
+++ merge-strings-alternately.js
@@ -6,16 +6,15 @@
 var mergeAlternately = function (word1, word2) {
   let result = "";
 
-  const maxlen = Math.max(word1.length + word2.length);
+  const maxlen = Math.max(word1.length, word2.length);
 
   for (let i = 0; i < maxlen; i++) {
     if (i < word1.length) {
       result += word1[i];
-    } else {
-      result += word2[i - word1.length];
+    }
+    if (i < word2.length) {
+      result += word2[i];
     }
   }
-  console.log(result);
+  return result;
 };
-
-mergeAlternately("Mareg", "back");
